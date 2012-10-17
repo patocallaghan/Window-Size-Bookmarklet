e0f33eb6fcc46f42e3a79978c30aa95594595fbf
Pat O'Callaghan <patocallaghan@gmail.com>
2012-10-17 22:06:54 +1300

    Refactoring and added Grunt JS
    Added Grunt process build
    Moved minified files from root to /dist directory
    Removed redundant files
    Updated .gitignore

---
+++ grunt.js
@@ -0,0 +1,89 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		meta: {
+			version: '0.1.0',
+			banner: '/*! PROJECT_NAME - v<%= meta.version %> - ' +
+				'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+				'* http://PROJECT_WEBSITE/\n' +
+				'* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+				'YOUR_NAME; Licensed MIT */'
+		},
+		lint: {
+			files: ['grunt.js', 'src/**/*.js', 'test/**/*.js']
+		},
+		test: {
+			files: ['test/**/*.js']
+		},
+		concat: {
+			dist: {
+				src: ['<banner:meta.banner>', '<file_strip_banner:src/FILE_NAME.js>'],
+				dest: 'dist/FILE_NAME.js'
+			}
+		},
+		compass: {
+			dev: {
+				src: 'src/css/scss',
+				dest: 'src/css',
+				outputstyle: 'expanded',
+				linecomments: true
+			},
+			prod: {
+				src: 'src/css/scss',
+				dest: 'src/css',
+				outputstyle: 'compressed',
+				linecomments: false,
+				forcecompile: true
+			}
+		},
+		min: {
+			bootstrap: {
+				src: ["src/js/bootstrap.js"],
+				dest: 'dist/bootstrap.min.js'
+			},
+			windowsize: {
+				src: ["src/js/windowsize.js"],
+				dest: 'dist/windowsize.min.js'
+			}
+		},
+		watch: {
+			files: '<config:lint.files>',
+			tasks: 'lint test',
+			min: {
+				files: ['src/js/*.js'],
+				tasks: ['min']
+			},
+			compass: {
+				files: ['css/scss/**/*.scss'],
+				tasks: ['compass:dev']
+			}
+		},
+		jshint: {
+			options: {
+				/*curly: true,
+				eqeqeq: true,
+				immed: true,
+				latedef: true,
+				newcap: true,
+				noarg: true,
+				sub: true,
+				undef: true,
+				boss: true,
+				eqnull: true*/
+			},
+			globals: {}
+		},
+		server: {
+			port: 8000,
+			base: 'src'
+		}
+	});
+
+	// Default task.
+	grunt.registerTask('default', 'lint test concat min compass:dev');
+
+	grunt.loadNpmTasks('grunt-compass');
+
+};
